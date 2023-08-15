bcf4afc336c0ce8e7935c72bde8f05a7a20739fc
Peak <pglund18@yahoo.com>
2023-08-14 21:12:04 -0500

    Search bar works

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,14 +1,13 @@
 const axios = require('axios'); 
 const express = require('express');
-const app = express();
-const port = 4000;
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
+        const { query } = req.query;
         const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
             params: {
-                query: 'Barbie',
+                query: query,
                 api_key: '278e8c312301aad59f629f56b6f2532a',
             },
         });
@@ -22,27 +21,6 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/test-api', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-                query: 'Barbie',
-                api_key: '278e8c312301aad59f629f56b6f2532a',
-            },
-        });
-
-        const movies = response.data.results;
-
-        res.render('page', { pageTitle: 'Test API Page', movies });
-    } catch (error) {
-        console.error('Error fetching data from API:', error);
-        res.status(500).send('Error fetching data from API');
-    }
-});
 
 module.exports = router;
 
-// Start the server
-// //app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-// });//
--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-// const express = require('express');
-// const exphbs = require('express-handlebars'); 
-// const movieController = require('./controllers/movieControllers'); 
-// const path = require('path');
-
-// const app = express();
-// const port = 3000;
-
-// //Handlebars
-// const hbs = exphbs.create({
-//     extname: 'handlebars',
-// });
-
-// app.engine('handlebars', hbs.engine); 
-// app.set('view engine', 'handlebars');
-
-// //static folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(require('./controllers'));
-
-// //render main.handlebars
-// // app.get('/', (req, res) => {
-// //     res.render('layouts/main', { pageTitle: 'My Movie Review Website' });
-// // });
-
-// //server
-// app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-// });
-
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
