b522e7de504870ccc1e48c96c042e984a513635b
ivanapina <ivanapina@gmail.com>
2023-08-18 10:50:01 -0500

    added some loginc to the comments, controllers

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -23,4 +23,3 @@ router.get('/', async (req, res) => {
 
 
 module.exports = router;
-
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
-const movieRoutes = require('./movieControllers') 
-router.use('/', movieRoutes )
+const movieRoutes = require('./api/movieRoutes') 
+const userRoutes = require('./api/userRoutes')
+const commentRoutes = require('./api/commentRoutes') 
+
+router.use('/', movieRoutes, userRoutes, commentRoutes )
 module.exports = router
